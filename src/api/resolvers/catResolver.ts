2dd2fba8f3484b9c6a04148fc92162d71a402e36
TapioAJ <104069226+TapioAJ@users.noreply.github.com>
2024-02-28 13:54:33 +0200

    First commit

---
+++ src/api/resolvers/catResolver.ts
@@ -0,0 +1,65 @@
+import { GraphQLError } from 'graphql';
+import catModel from '../models/catModel';
+import { Cat, CatInput, CatModify, LocationInput, TokenContent } from '../../types/DBTypes';
+import mongoose from 'mongoose';
+import { MyContext } from '../../types/MyContext';
+import { isLoggedIn } from '../../functions/authorize';
+
+
+// TODO: create resolvers based on cat.graphql
+// note: when updating or deleting a cat, you need to check if the user is the owner of the cat
+// note2: when updating or deleting a cat as admin, you need to check if the user is an admin by checking the role from the user object
+// note3: updating and deleting resolvers should be the same for users and admins. Use if statements to check if the user is the owner or an admin
+
+const catResolver = {
+    Query: {
+        catById: async (_parent: undefined, args: Cat) => {
+            return await catModel.findById(args.id);
+        },
+        cats: async () => {
+            return await catModel.find();
+        },
+        catsByArea: async (_parent: undefined, args: LocationInput) => {
+            return await catModel.find({
+                location: {
+                    $geoWithin: {
+                        $box: [
+                            [args.bottomLeft.lng, args.bottomLeft.lat],
+                            [args.topRight.lng, args.topRight.lat],
+                        ],
+                    },
+                },
+            });
+        },
+        catsByOwner: async (_parent: undefined, args: { id: string }) => {
+            return await catModel.find({ owner: args.id });
+        }
+    },
+    Mutation: {
+        createCat: async (_parent: undefined, args: { input: Omit<Cat, 'id'> }, context: MyContext) => {
+            isLoggedIn(context);
+            args.input.owner = context.userdata?.user.id;
+            return await catModel.create(args.input);
+        },
+        updateCat: async (_parent: undefined, args: { id: string; input: Partial<Omit<Cat, 'id'>> }, context: MyContext,) => {
+            isLoggedIn(context);
+            if (context.userdata?.user.role !== 'admin') {
+                const filter = { _id: args.id, owner: context.userdata?.user.id };
+                return await catModel.findOneAndUpdate(filter, args.input, { new: true });
+            } else {
+                return await catModel.findByIdAndUpdate(args.id, args.input, { new: true });
+            }
+        },
+        deleteCat: async (_parent: undefined, args: { id: string; }, context: MyContext) => {
+            isLoggedIn(context);
+            if (context.userdata?.user.role !== 'admin') {
+                const filter = { _id: args.id, owner: context.userdata?.user.id };
+                return await catModel.findOneAndDelete(filter);
+            } else {
+                return await catModel.findByIdAndDelete(args.id);
+            }
+        },
+    },
+};
+
+export default catResolver;
---
+++ src/api/resolvers/userResolver.ts
@@ -0,0 +1,95 @@
+import {Cat, Credentials, TokenContent, User, UserInput, UserOutput} from '../../types/DBTypes';
+import fetchData from '../../functions/fetchData';
+import {LoginResponse, UserResponse} from '../../types/MessageTypes';
+import { MyContext } from '../../types/MyContext';
+import { isLoggedIn, isAdmin } from '../../functions/authorize';
+
+// TODO: create resolvers based on user.graphql
+// note: when updating or deleting a user don't send id to the auth server, it will get it from the token. So token needs to be sent with the request to the auth server
+// note2: when updating or deleting a user as admin, you need to send user id (dont delete admin btw) and also check if the user is an admin by checking the role from the user object form context
+
+
+const userResolver = {
+    Cat: {
+        owner: async (parent: Cat) => {
+            return await fetchData<User>(`${process.env.AUTH_URL}/users/${parent.owner}`);
+        },
+    },
+    Query: {
+        users: async () => {
+            return await fetchData<User[]>(`${process.env.AUTH_URL}/users`);
+        },
+        userById: async (_parent: undefined, args: {id: string}) => {
+            return await fetchData<User>(`${process.env.AUTH_URL}/users/${args.id}`);
+        },
+        checkToken: async (_parent: undefined, _args: undefined, context: MyContext) => {
+            return await {user: context.userdata?.user};
+        },
+    },
+    Mutation:{
+        login: async (_parent: undefined, args: {credentials:Credentials}) => {
+            return await fetchData<LoginResponse>(`${process.env.AUTH_URL}/auth/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(args.credentials),
+            });
+        },
+        register: async (_parent: undefined, args: {user: UserInput}) => {
+            return await fetchData<UserResponse>(`${process.env.AUTH_URL}/users`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(args.user),
+            });
+        },
+        updateUser: async (_parent: undefined, args: {user: UserInput}, context: MyContext) => {
+            isLoggedIn(context);
+            return await fetchData<UserResponse>(`${process.env.AUTH_URL}/users`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${context.userdata?.token}`,
+                },
+                body: JSON.stringify(args.user),
+            });
+        },
+        deleteUser: async (_parent: undefined, args: undefined, context: MyContext) => {
+            isLoggedIn(context);
+            return await fetchData<UserResponse>(`${process.env.AUTH_URL}/users`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${context.userdata?.token}`,
+                },
+            });
+        },
+        updateUserAsAdmin: async (_parent: undefined, args: {user: UserInput, id: string}, context: MyContext) => {
+            isAdmin(context);
+            return await fetchData<UserResponse>(`${process.env.AUTH_URL}/users/${args.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${context.userdata?.token}`,
+                },
+                body: JSON.stringify(args.user),
+            });
+        },
+        deleteUserAsAdmin: async (_parent: undefined, args: {id: string}, context: MyContext) => {
+            isAdmin(context);
+            return await fetchData<UserResponse>(`${process.env.AUTH_URL}/users/${args.id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${context.userdata?.token}`,
+                },
+            });
+        },
+
+    }
+
+};
+
+export default userResolver;
